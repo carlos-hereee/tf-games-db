a1193b99b717b7c7b84b9f10b26ce6c2b0f8f609
ch04937 <97hernandez.c@gmail.com>
2022-02-03 12:15:48 -0800

    keep track score of diagnol moves

--- live-servers/combination.js
+++ live-servers/combination.js
@@ -1,6 +1,6 @@
 const checkVictory = (board, player, count) => {
-  let scoreBoard = { x1: 2, x2: 0, x3: 0, y1: 0, y2: 0, y3: 0, z1: 0, z2: 0 };
-  if (count + 1 > 8) return "draw";
+  let scoreBoard = { x1: 0, x2: 0, x3: 0, y1: 0, y2: 0, y3: 0, z1: 0, z2: 0 };
+  if (count > 8) return "draw";
   // update the scoreboard
   for (let i = 0; i < board.length; i++) {
     const cell = board[i];
@@ -9,12 +9,12 @@ const checkVictory = (board, player, count) => {
       scoreBoard[`x${cell.x}`] += 1;
       scoreBoard[`y${cell.y}`] += 1;
       // log diagnols
-      if (cell.x === 1 && cell.y === 1) scoreBoard.z1 + 1;
-      if (cell.x === 2 && cell.y === 2) scoreBoard.z1 + 1;
-      if (cell.x === 3 && cell.y === 3) scoreBoard.z1 + 1;
-      if (cell.x === 1 && cell.y === 3) scoreBoard.z2 + 1;
-      if (cell.x === 2 && cell.y === 2) scoreBoard.z2 + 1;
-      if (cell.x === 3 && cell.y === 1) scoreBoard.z2 + 1;
+      if (cell.x === 1 && cell.y === 1) scoreBoard.z1 += 1;
+      if (cell.x === 2 && cell.y === 2) scoreBoard.z1 += 1;
+      if (cell.x === 3 && cell.y === 3) scoreBoard.z1 += 1;
+      if (cell.x === 1 && cell.y === 3) scoreBoard.z2 += 1;
+      if (cell.x === 2 && cell.y === 2) scoreBoard.z2 += 1;
+      if (cell.x === 3 && cell.y === 1) scoreBoard.z2 += 1;
     }
   }
   if (Object.values(scoreBoard).includes(3)) {
