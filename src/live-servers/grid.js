d6c9d32bca45ae87bae97841418123acdf37c748
ch04937 <97hernandez.c@gmail.com>
2022-09-14 13:55:09 -0700

    create grid functions

---
+++ src/live-servers/grid.js
@@ -0,0 +1,50 @@
+const { v4: uuidv4 } = require("uuid");
+
+const createGrid = ({ length, width, content }) => {
+  let board = [];
+  for (let x = 1; x < length + 1; x++) {
+    for (let y = 1; y < width + 1; y++) {
+      if (x === Math.floor(length / 2) && y === Math.floor(width / 2)) {
+        board.push({
+          x,
+          y,
+          isEmpty: false,
+          content: content,
+          uid: uuidv4(),
+        });
+      } else {
+        board.push({
+          x,
+          y,
+          isEmpty: true,
+          content: "",
+          uid: uuidv4(),
+        });
+      }
+    }
+  }
+  return board;
+};
+
+const randomGridPosition = ({ length, width, height }) => {
+  return {
+    x: Math.floor(Math.random() * width) + 1,
+    y: Math.floor(Math.random() * length) + 1,
+    z: Math.floor(Math.random() * height || 0) + 1,
+  };
+};
+const outsideGrid = (position, gridSize) => {
+  return (
+    position.x < 1 ||
+    position.x > gridSize.x ||
+    position.y < 1 ||
+    position.y > gridSize.y
+  );
+};
+
+const grid = {
+  tictactoe: (init) => createGrid(init),
+  snakeGame: (init) => createGrid(init),
+};
+
+module.exports = { grid, randomGridPosition, outsideGrid };
