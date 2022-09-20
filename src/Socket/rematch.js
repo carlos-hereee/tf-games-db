f404a6c6bb855ddff271a4416a288d34e403b9ca
ch04937 <97hernandez.c@gmail.com>
2022-09-19 20:44:36 -0700

    restart game timer

--- src/Socket/rematch.js
+++ src/Socket/rematch.js
@@ -1,5 +1,6 @@
 const { resetGame } = require("../live-servers/game");
 const { emitRematch, emitResetData } = require("../live-servers/socketEmit");
+const { startGameTimer } = require("./timer");
 
 const rematch = (s, { game, player }) => {
   const isPlayer1 = game.player1.uid === player.uid;
@@ -11,6 +12,7 @@ const rematch = (s, { game, player }) => {
   if (game.player2.rematch && game.player1.rematch) {
     // send reset game to both player
     const { reset } = resetGame(game);
+    startGameTimer(s, reset);
     emitResetData(s, reset);
   } else {
     emitRematch(s, game, isPlayer1);
