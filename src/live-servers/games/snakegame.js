1e66f23f19fa481581fa76afb9d579558cd8a226
ch04937 <97hernandez.c@gmail.com>
2022-09-21 12:20:41 -0700

    DRY code

--- src/live-servers/games/snakegame.js
+++ src/live-servers/games/snakegame.js
@@ -8,30 +8,55 @@ const {
 const snakegame = (s, game, motion, _) => {
   let { board, snakeBody, options, food } = game;
 
-  if (game.gameOver) return { g: game };
-  const currentTime = Date.now();
-  const sinceLastTime = (currentTime - game.lastTimeRender) / 1000;
-  if (sinceLastTime < 1 / options.snakeSpeed) return { g: game };
-  game.lastTimeRender = currentTime;
-
-  for (i = 0; i < options.newSegment; i++) {
-    snakeBody.push([{ ...snakeBody[snakeBody.length - 1] }]);
+  if (game.gameOver) return;
+  checkSegments(board, snakeBody, options, motion);
+  checkDeath(game, snakeBody, options);
+  checkFood(board, snakeBody, options, food);
+  updateBoard(board, snakeBody, food);
+};
+const updateBoard = (board, snakeBody, food) => {
+  for (let i = 0; i < board.length; i++) {
+    const snakeMatch = snakeBody.some((s) => checkCellsMatch(board[i], s));
+    const foodMatch = food.some((f) => checkCellsMatch(board[i], f));
+    // if the board cell match snakebody cell
+    if (snakeMatch) {
+      const idx = snakeBody.findIndex((s) => checkCellsMatch(board[i], s));
+      updateGrid(board, snakeBody[idx], "snake");
+    }
+    // if food cell match board cell
+    if (foodMatch) {
+      const idx = food.findIndex((f) => checkCellsMatch(board[i], f));
+      updateGrid(board, food[idx], "food");
+    }
+    // erase everything else on board
+    if (!snakeMatch && !foodMatch) {
+      updateGrid(board, board[i], "");
+    }
   }
-  options.newSegment = 0;
+};
+const checkCellsMatch = (cell1, cell2) => {
+  return cell1.x === cell2.x && cell1.y === cell2.y;
+};
+const checkSegments = (_, snakeBody, options, motion) => {
+  // add segment
+  if (options.newSegment) {
+    for (i = 0; i < options.newSegment; i++) {
+      snakeBody.push([{ ...snakeBody[snakeBody.length - 1] }]);
+    }
+    options.newSegment = 0;
+  }
+  // move body
   for (let i = snakeBody.length - 2; i >= 0; i--) {
+    console.log("snakeBody -length 2", i);
     snakeBody[i + 1] = { ...snakeBody[i] };
   }
-  const head = { x: snakeBody[0].x + motion.x, y: snakeBody[0].y + motion.y };
-  const headIdx = findCellIdx(board, head);
-  snakeBody[0] = board[headIdx];
-  options.lastInputDirection = motion;
-
-  // check death
-  if (outsideGrid(snakeBody[0], options.size) || isCrash(snakeBody)) {
-    game.gameOver = true;
-    game.gameResult = "defeat";
-    return { g: game };
-  }
+  // const head = { x: snakeBody[0].x + motion.x, y: snakeBody[0].y + motion.y };
+  // const idx = findCellIdx(board, head);
+  // snakeBody[0] = board[idx];
+  snakeBody[0].x += motion.x;
+  snakeBody[0].y += motion.y;
+};
+const checkFood = (board, snakeBody, options, food) => {
   // update food
   for (let i = 0; i < food.length; i++) {
     if (onSnake(snakeBody, food[i])) {
@@ -39,29 +64,15 @@ const snakegame = (s, game, motion, _) => {
       options.newSegment += options.expansionRate;
       const coords = getRandomFoodPostion(board, snakeBody, options.size);
       food.push(coords);
-      board = updateGrid(board, coords, "food");
     }
   }
-
-  // // update board
-  for (let i = 0; i < board.length; i++) {
-    // draw snake body on board
-    if (snakeBody.some((s) => s.uid === board[i].uid)) {
-      const idx = snakeBody.findIndex((s) => s.uid === board[i].uid);
-      board = updateGrid(board, snakeBody[idx], "snake");
-      // draw food on board
-    } else if (food.some((f) => f.uid === board[i].uid)) {
-      const idx = food.findIndex((s) => s.uid === board[i].uid);
-      board = updateGrid(board, food[idx], "food");
-      // erase everything else on board
-    } else if (board[i].content) {
-      updateGrid(board, board[i], "");
-    }
-  }
-
-  return { g: game };
 };
-
+const checkDeath = (game, snakeBody, options) => {
+  if (outsideGrid(snakeBody[0], options.size) || isCrash(snakeBody)) {
+    game.gameOver = true;
+    game.gameResult = "defeat";
+  }
+};
 const getRandomFoodPostion = (grid, snakeBody, size) => {
   let newFoodPosition;
   while (!newFoodPosition || onSnake(snakeBody, newFoodPosition)) {
@@ -76,7 +87,6 @@ const onSnake = (snakeBody, position, { ignoreHead = false } = {}) => {
     return s?.x === position?.x && s?.y === position?.y;
   });
 };
-const isCrash = (snakeBody) => {
-  return onSnake(snakeBody, snakeBody[0], { ignoreHead: true });
-};
+
+const isCrash = (sb) => onSnake(sb, sb[0], { ignoreHead: true });
 module.exports = { snakegame, getRandomFoodPostion };
